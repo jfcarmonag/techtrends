98efd65313e8bad535c52accda0a78b899045b7d
Juan_Felipe <jfcarmonag@gmail.com>
2021-07-20 10:55:58 -0500

    github workflow

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,34 @@
 from flask import Flask
+from flask import json
+import logging
+
 app = Flask(__name__)
 
+@app.route("/status")
+def healthcheck():
+    response = app.response_class(
+        response=json.dumps({"result":"OK - Healthy"}),
+        status = 200,
+        mimetype = 'application/json'
+    )
+    app.logger.info('status request succesful')
+    return response
+
+@app.route("/metrics")
+def metrics():
+    response = app.response_class(
+        response= json.dumps({"status": "success", "code":0, "data":{"UserCount":140,"UserCountActive":23}}),
+        status= 200,
+        mimetype=  'application/json'
+    )
+
+    app.logger.info('metrics request succesful')
+    return response
 @app.route("/")
 def hello():
+    app.logger.info('main request succesful')
     return "Hello World!"
 
 if __name__ == "__main__":
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
     app.run(host='0.0.0.0')
--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,3 +1,4 @@
+import logging
 import sqlite3
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
@@ -5,10 +6,21 @@ from werkzeug.exceptions import abort
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
+
+connection_count=0
+
 def get_db_connection():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    global connection_count
+    connection_count += 1
     return connection
+
+def count_posts():
+    connection = get_db_connection()
+    p_count = connection.execute('SELECT Count(*) FROM posts').fetchone()[0]
+    connection.close()
+    return p_count
 
 # Function to get a post using its ID
 def get_post(post_id):
@@ -36,13 +48,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      app.logger.info('Non existing article, 404 returned')
       return render_template('404.html'), 404
     else:
-      return render_template('post.html', post=post)
+       app.logger.info(post['title'])
+       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info('AboutUs page retreived')
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -60,11 +75,49 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
-
+            app.logger.info('"{}" created'.format(title))
             return redirect(url_for('index'))
 
     return render_template('create.html')
+#Healthcheck endpoint
+@app.route('/healthz')
+def healthcheck():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    ## log line
+    app.logger.info('Status request successful')
+    return response
+#Metrics endpoint
+@app.route('/metrics')
+def metrics():
+    p_count = count_posts()
+    response = app.response_class(
+            response=json.dumps({"db_connection_count": connection_count, "post_count": p_count}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    ## log line
+    app.logger.info('Metrics request successful')
+    return response
 
 # start the application on port 3111
 if __name__ == "__main__":
+   #stream logs to a file
+   import sys
+
+   file_handler = logging.FileHandler(filename='app.log')
+   stdout_handler = logging.StreamHandler(sys.stdout)
+   stderr_handler = logging.StreamHandler(sys.stderr)
+   handlers = [file_handler, stdout_handler,stderr_handler]
+
+   logging.basicConfig(
+      level=logging.DEBUG, 
+      format='[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s',
+      handlers=handlers)
+   
    app.run(host='0.0.0.0', port='3111')
